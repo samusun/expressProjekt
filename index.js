c807f9e8a457f41075aedd9b54ee9709388f1231
samusun <samuelsinkorg@gmail.com>
2021-11-10 14:58:01 +0100

    added azure again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { mockdbDriver } from './drivers/mockdbdriver.js';
 import { mongoDriver } from './drivers/mongodriver.js';
 
 const PORT = process.env.PORT || 3000; // 3000 or 80
-const DBTYPE = process.env.DBTYPE || 'moock'; // mockormongo
+const DBTYPE = process.env.DBTYPE || 'mock'; // mockormongo
 const DBCONN = process.env.CONNECTION_STRING || '<default>';
 const DBNAME = process.env.DBNAME || '<default>';
 
